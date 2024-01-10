4bc8a9fb18508be36a18921fc707552cf47d55bc
Sergei <sergei.shimanovich@gmail.com>
2024-01-10 21:33:38 +0300

    Classwork task 1.1

---
+++ 1_lesson/1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int a = 25;
+int b = 5;
+
+if (b * b == a)
+    Console.WriteLine ("true");
+else
+    Console.WriteLine ("false");
+
+Console.Write("Enter a number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(num1 * num1);        
