5916611b9aaf7a1e6d44837d8415c222aaae23b1
Sergei <sergei.shimanovich@gmail.com>
2024-01-12 01:08:28 +0300

    Homework task 2.2

---
+++ 2_homework/2.2/Program.cs
@@ -0,0 +1,7 @@
+﻿int x = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+if (x > 0 && y > 0){Console.WriteLine("one");}
+if (x < 0 && y > 0){Console.WriteLine("two");}
+if (x < 0 && y < 0){Console.WriteLine("three");}
+if (x > 0 && y < 0){Console.WriteLine("four");}
+else {Console.WriteLine(x == 0 && y == 0);}
