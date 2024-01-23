c0244e08b4efc8176e34016a123c8291442ed643
Sergei <sergei.shimanovich@gmail.com>
2024-01-23 21:03:37 +0300

    3_lesson task 3.1

---
+++ 3_lesson/3.1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = {1, 3, 4, 19, 3, -4, -8, 5};
+int a = int.Parse(Console.ReadLine()!);
+bool b = false;
+for (int i = 0; i < arr.Length; i++)
+{
+  if(a == arr[i])
+  b = true;
+ 
+}
+if (b)
+Console.Write("Yes");
+
+   else
+   Console.Write("No");
