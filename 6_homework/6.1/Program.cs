d6456c167e8382b26e25c6ff018969ac46e45690
Sergei <sergei.shimanovich@gmail.com>
2024-02-03 21:54:52 +0300

    added task 6.1

---
+++ 6_homework/6.1/Program.cs
@@ -0,0 +1,17 @@
+﻿char[,] charArr = new char[,] { {'R','e','a','d'}, {'L','i','n','e'}};
+string result = Concat(charArr);
+Console.WriteLine(result);
+
+string Concat(char[,] arr)
+{
+    string result = " ";
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            result += arr[i, j];    
+        }
+    }
+    return result;
+}
+
