2c9a2a3d1f800d43e107131a20fa6b3dc237220b
Sergei <sergei.shimanovich@gmail.com>
2024-02-03 18:26:59 +0300

    added task 5.1

---
+++ 5_lesson/5.1/Program.cs
@@ -0,0 +1,14 @@
+﻿string Concat(char[] mass)
+{
+    string buf = " ";
+    for (int i = 0; i < mass.Length; i++)
+    {
+        buf = buf + mass[i];
+    }
+    return buf;
+}
+
+char[] charArr = {'H', 'e', 'l', 'l', 'o'};
+
+string result = Concat(charArr);
+Console.WriteLine(result);
