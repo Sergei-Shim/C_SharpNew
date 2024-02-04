02ca303cb8df5367e1067aa4ee67d145ab54a439
Sergei <sergei.shimanovich@gmail.com>
2024-02-04 21:33:47 +0300

    added task 6.3

---
+++ 6_homework/6.3/Program.cs
@@ -0,0 +1,19 @@
+﻿//3.Задайте произвольную строку. Выясните, является
+// ли она палиндромом
+
+bool Palindrome(string word)
+{
+    int size = word.Length;
+    for (int i = 0; i < size / 2; i++)
+    {
+        if (word[i] != word[size - 1 - i])
+            return false;
+    }
+    return true;
+}
+
+Console.Write("Ввести строку: ");
+string str = Console.ReadLine()!;
+
+bool result = Palindrome(str);
+Console.WriteLine($"Строка - палиндром - {result}"); 
